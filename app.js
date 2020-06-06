a452d4e62296bd2dc9ee6507a80c3145aca9eb14
Asra Iqbal <asraiqbal300@gmail.com>
2020-06-06 22:03:27 +0500

    deleted extra files

--- app.js
+++
@@ -1,9 +0,0 @@
-function menuToggle() {
-    var el = document.getElementById("mob-opts")
-    if(el.classList.contains("show")){
-        el.classList.remove("show")
-    }
-    else{
-        el.classList.add("show")
-    }
-}
